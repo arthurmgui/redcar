cb55bc2331b616a967f82c6edfc4235c2099647d
arthurmgui <arthurmottaguimaraes@outlook.com>
2023-10-31 12:25:52 -0300

    acrescentando scrollview

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    <ScrollView style={{flex: 1}}>
     <View style={styles.container}>
       <Text>Hello world!!!</Text>
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    margin: 100,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
