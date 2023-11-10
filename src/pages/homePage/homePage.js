3756b6aa8eb1fabdd285c881dbbf2bf8d903d910
arthurmgui <arthurmottaguimaraes@outlook.com>
2023-11-10 19:21:35 -0300

    refatorando para iniciar o app de forma mais organizada

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Text, View, Image, Dimensions, StyleSheet, ScrollView, SafeAreaView, TextInput, Alert, Button, KeyboardAvoidingView, TouchableOpacity, Pressable} from "react-native";
+import { Text, View, Image, Dimensions, StyleSheet, ScrollView, SafeAreaView, KeyboardAvoidingView, Pressable} from "react-native";
 import redcar from '../../../assets/redcar_tela_2.jpg';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const width = Dimensions.get('screen').width;
 
@@ -13,56 +14,86 @@ export default function HomePage() {
     const [inputClientSearch, onChangeInputClientSearch] = React.useState('');
 
     return  (
-        <SafeAreaView>
-            <Image source={redcar} style={styleRedCar.topo} />
-            <View style={styleRedCar.appBar}>
-                <Text style={styleRedCar.title}>RedCar</Text>
-                <TextInput
-                    style={styleRedCar.inputSearch}
-                    onChangeText={onChangeInputClientSearch}
-                    value={inputClientSearch}
-                    placeholder="Pesquise o Cliente"
-                /> 
+        <SafeAreaView style={styleRedCar.safeAreaStyle}>
+            <View style={styleRedCar.viewContainer}>
+                <Image source={redcar} style={styleRedCar.topo} />
+                <View style={styleRedCar.appBar}>
+                    <Text style={styleRedCar.title}>RedCar</Text>
+                </View>
+                <ScrollView>
+                    <KeyboardAvoidingView>
+                        <View>
+                            <Text style={styleRedCar.quicklyAcessTxt}>Acesso rápido</Text>
+                            <View style={styleRedCar.containerQuiclyCards}>
+                                <View style={styleRedCar.containerCards}>
+                                    <Pressable style={styleRedCar.card}>
+                                        {({pressed}) => (
+                                            <View style={styleRedCar.iconCard}>
+                                                <Icon name="user" size={20} color="white" style={styleRedCar.iconCard}/>
+                                            </View>
+                                        )}
+                                    </Pressable>
+                                    <Text style={styleRedCar.textCard}>
+                                        clientes
+                                    </Text>
+                                </View>
+                                <View style={styleRedCar.containerCards}>
+                                    <Pressable style={styleRedCar.card}>
+                                        {({pressed}) => (
+                                            <View style={styleRedCar.iconCard}>
+                                                <Icon name="car" size={20} color="white" style={styleRedCar.iconCard}/>
+                                            </View>
+                                        )}
+                                    </Pressable>
+                                    <Text style={styleRedCar.textCard}>
+                                        garagem
+                                    </Text>
+                                </View>
+                                <View style={styleRedCar.containerCards}>
+                                    <Pressable style={styleRedCar.card}>
+                                        {({pressed}) => (
+                                            <View style={styleRedCar.iconCard}>
+                                                <Icon name="money" size={20} color="white" style={styleRedCar.iconCard}/>
+                                            </View>
+                                        )}
+                                    </Pressable>
+                                    <Text style={styleRedCar.textCard}>
+                                        orçamento
+                                    </Text>
+                                </View>
+                            </View>
+                        </View>
+                    </KeyboardAvoidingView>
+                </ScrollView>
             </View>
-            <ScrollView>
-                <KeyboardAvoidingView>
-                    <Text style={styleRedCar.singupTitle}>Novo cliente</Text>
-                    <TextInput
-                        style={styleRedCar.input}
-                        onChangeText={onChangeClientName}
-                        value={clientName}
-                        placeholder= "Nome"
-                    />
-                    <TextInput
-                        style={styleRedCar.input}
-                        onChangeText={onChangeClientAdress}
-                        value={clientAdress}
-                        placeholder= "Endereço"
-                    />
-                    <TextInput
-                        style={styleRedCar.input}
-                        onChangeText={onChangeClientContact}
-                        value={clientContact}
-                        placeholder= "Contato"
-                        keyboardType= "number-pad"
-                    />
-                    <View style={styleRedCar.containerButton}>
-                        <Pressable style={styleRedCar.buttonCreate}>
-                            {({pressed}) => (
-                                <Text style={[styleRedCar.textButtonCreate, pressed && styleRedCar.textButtonCreate]}>Cadastrar cliente</Text>
-                            )}
-                        </Pressable>
-                    </View>
-                </KeyboardAvoidingView>
-            </ScrollView>
-            </SafeAreaView>
+        </SafeAreaView>
     )
 }
 
 const styleRedCar = StyleSheet.create({
+
+    safeAreaStyle: {
+        flex: 1,
+        justifyContent: "space-between",
+        backgroundColor: "#bd2a2a",
+    },
+
+    viewContainer: {
+        flex: 1,
+        justifyContent: "space-between",
+        backgroundColor: "#bd2a2a",
+    },
+
     topo: {
         width: "100%",
-        height: 400 / 680 * width,
+        height: 280 / 900 * width,
+        borderBottomRightRadius: 30,
+        borderBottomLeftRadius: 30,
+    },
+
+    appBar: {
+        position: "absolute",
+        width: "100%"
     },
 
     title: {
@@ -78,61 +109,49 @@ const styleRedCar = StyleSheet.create({
         textShadowRadius: 20, // Raio da sombra
     },
 
-    singupTitle: {
+    quicklyAcessTxt: {
         width: "100%",
-        textAlign: "center",
-        fontSize: 23,
-        color: "red",
+        fontSize: 18,
+        color: "white",
         fontWeight: "bold",
         padding: 17,
     },
 
-    input: {
-        height: 45,
-        margin: 8,
-        color: "black",
-        borderWidth: 2,
-        borderColor: "red",
-        padding: 10,
-        borderTopLeftRadius: 7,
-        borderTopRightRadius: 7,
-        borderBottomRightRadius: 7,
-        borderBottomLeftRadius: 7,
-    },
-
-    inputSearch: {
-        height: 60,
-        margin: 8,
-        color: "white",
-        backgroundColor: '#8e1e1ef6',
-        borderWidth: 2,
-        borderColor: 'rgba(300, 0, 0, 0.8)',
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        borderBottomRightRadius: 20,
-        borderBottomLeftRadius: 20,
-        padding: 15,
-        shadowColor: "red",
-        shadowOffset: { width: 2, height: 2},
-        shadowRadius: 20,
-    },
-
-    appBar: {
-        position: "absolute",
-        width: "100%"
-    },
-
-    containerButton: {
+    containerQuiclyCards: {
         flex: 1,
-        justifyContent: "center",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
         alignItems: "center",
     }, 
 
-    buttonCreate: {
-        backgroundColor: "red",
-        width: "50%",
-        padding: 10,
+    containerCards: {
+        flex: 1,
+        alignItems: "center",
+    },
+
+    card: {
+        backgroundColor: "#db4444",
+        width: "85%",
+        padding: 17,
         borderRadius: 5,
+        elevation: 5,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+    },
+
+    cardPressed: {
+        backgroundColor: "#f35757",
+    },
+
+    iconCard: {
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    textCard: {
+        color: "white"
     },
 
     textButtonCreate: {
