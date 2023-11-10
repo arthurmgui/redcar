5d3f3073b10725bcfba11017f93f8ed743eab366
arthurmgui <arthurmottaguimaraes@outlook.com>
2023-11-10 09:53:38 -0300

    alterando campos que serão usados para o cliente

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -10,8 +10,6 @@ export default function HomePage() {
     const [clientName, onChangeClientName] = React.useState('');
     const [clientAdress, onChangeClientAdress] = React.useState('');
     const [clientContact, onChangeClientContact] = React.useState('');
-    const [clientCar, onChangeClientCar] = React.useState('');
-    const [carLicensePlate, onChangeCarLicensePlate] = React.useState('');
     const [inputClientSearch, onChangeInputClientSearch] = React.useState('');
 
     return  (
@@ -24,50 +22,38 @@ export default function HomePage() {
                     onChangeText={onChangeInputClientSearch}
                     value={inputClientSearch}
                     placeholder="Pesquise o Cliente"
-                />
+                /> 
             </View>
             <ScrollView>
-                    <KeyboardAvoidingView>
-                        <Text style={styleRedCar.singupTitle}>Novo cliente</Text>
-                        <TextInput
-                            style={styleRedCar.input}
-                            onChangeText={onChangeClientName}
-                            value={clientName}
-                            placeholder= "Nome"
-                        />
-                        <TextInput
-                            style={styleRedCar.input}
-                            onChangeText={onChangeClientAdress}
-                            value={clientAdress}
-                            placeholder= "Endereço"
-                        />
-                        <TextInput
-                            style={styleRedCar.input}
-                            onChangeText={onChangeClientContact}
-                            value={clientContact}
-                            placeholder= "Contato"
-                            keyboardType= "number-pad"
-                        />
-                        <TextInput
-                            style={styleRedCar.input}
-                            onChangeText={onChangeClientCar}
-                            value={clientCar}
-                            placeholder= "Modelo do carro"
-                        />
-                        <TextInput
-                            style={styleRedCar.input}
-                            onChangeText={onChangeCarLicensePlate}
-                            value={carLicensePlate}
-                            placeholder= "Placa do carro"
-                        />
-                        <View style={styleRedCar.containerButton}>
-                            <Pressable style={styleRedCar.buttonCreate}>
-                                {({pressed}) => (
-                                    <Text style={[styleRedCar.textButtonCreate, pressed && styleRedCar.textButtonCreate]}>Cadastrar cliente</Text>
-                                )}
-                            </Pressable>
-                        </View>
-                    </KeyboardAvoidingView>
+                <KeyboardAvoidingView>
+                    <Text style={styleRedCar.singupTitle}>Novo cliente</Text>
+                    <TextInput
+                        style={styleRedCar.input}
+                        onChangeText={onChangeClientName}
+                        value={clientName}
+                        placeholder= "Nome"
+                    />
+                    <TextInput
+                        style={styleRedCar.input}
+                        onChangeText={onChangeClientAdress}
+                        value={clientAdress}
+                        placeholder= "Endereço"
+                    />
+                    <TextInput
+                        style={styleRedCar.input}
+                        onChangeText={onChangeClientContact}
+                        value={clientContact}
+                        placeholder= "Contato"
+                        keyboardType= "number-pad"
+                    />
+                    <View style={styleRedCar.containerButton}>
+                        <Pressable style={styleRedCar.buttonCreate}>
+                            {({pressed}) => (
+                                <Text style={[styleRedCar.textButtonCreate, pressed && styleRedCar.textButtonCreate]}>Cadastrar cliente</Text>
+                            )}
+                        </Pressable>
+                    </View>
+                </KeyboardAvoidingView>
             </ScrollView>
             </SafeAreaView>
     )
